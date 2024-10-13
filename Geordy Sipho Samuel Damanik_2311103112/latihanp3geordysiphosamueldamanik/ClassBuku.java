d8063d74eb71fedcac451543dfe9c0601b64e6f7
Geordy Sipho Samuel Damanik <geordysipho341@gmail.com>
2024-10-13 23:55:15 +0700

    Add files via upload

---
+++ Geordy Sipho Samuel Damanik_2311103112/latihanp3geordysiphosamueldamanik/ClassBuku.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanp3geordysiphosamueldamanik;
+
+/**
+ *
+ * @author Geordy Sipho Samuel Damanik
+ * 2311103112
+ * S1SI-07-C
+ */
+
+public class ClassBuku {
+    String judul, penulis;
+    int tahunTerbit;
+    
+    public ClassBuku(String judul, String penulis, int tahunTerbit){
+        this.judul = judul;
+        this.penulis = penulis;
+        this.tahunTerbit = tahunTerbit;
+  }    
+}
